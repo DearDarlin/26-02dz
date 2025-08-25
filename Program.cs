2258283f20fc81e80004f5a5f083dbc05dabcc33
DearDarlin <darinakovalenko78@gmail.com>
2025-08-25 21:15:26 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+
+public static class Fibonacci
+{
+    public static bool IsFibonacci(int n)
+    {
+        if (n < 0) return false;
+        int a = 0, b = 1;
+        while (a < n)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a == n;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+        if (Fibonacci.IsFibonacci(number))
+        {
+            Console.WriteLine($"{number} is a Fibonacci number.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is not a Fibonacci number.");
+        }
+    }
+}
