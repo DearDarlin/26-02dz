97d384084bf153dc15289eecd1bb20b32c637bfc
DearDarlin <darinakovalenko78@gmail.com>
2025-08-25 22:48:06 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+using System.Linq;
+
+public record StudentGrades(string Name, int[] Grades);
+
+class Program
+{
+    static void Main()
+    {
+        var students = new[]
+        {
+            new StudentGrades("Mark", new[] { 12, 9, 7 }),
+            new StudentGrades("Bob", new[] { 6, 2, 9 }),
+            new StudentGrades("Darina", new[] { 12, 11, 12 })
+        };
+        var averageGrades = students
+            .Select(s => new
+            {
+                s.Name,
+                AverageGrade = s.Grades.Average()
+            });
+        foreach (var student in averageGrades)
+        {
+            Console.WriteLine($"{student.Name}: {student.AverageGrade:F2}");
+        }
+
+        Console.WriteLine($"Student with highest average: {averageGrades.OrderByDescending(s => s.AverageGrade).First().Name}");
+    }
+}
