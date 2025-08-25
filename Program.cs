8e419f9858c1615373fa19c255c5a4631864af63
DearDarlin <darinakovalenko78@gmail.com>
2025-08-25 21:39:51 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+public static class StringExtensions {
+    public static bool ContainsCaseSensitive(this string source, string toCheck)
+    {
+        return source.IndexOf(toCheck, StringComparison.Ordinal) >= 0;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        string source = "Hello World";
+        string toCheck1 = "world";
+        string toCheck2 = "World";
+        bool result1 = source.ContainsCaseSensitive(toCheck1);
+        bool result2 = source.ContainsCaseSensitive(toCheck2);
+        Console.WriteLine($"{toCheck1} - {result1}");
+        Console.WriteLine($"{toCheck2} - {result2}");
+    }
+}
