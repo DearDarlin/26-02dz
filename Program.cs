62bcd8e8b7361af4ddcccd08db0145931dd1e219
DearDarlin <darinakovalenko78@gmail.com>
2025-08-25 21:22:22 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+public static class StringExtensions
+{
+    public static string WordCount(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+        {
+            return "0 words";
+        }
+        var wordCount = str.Split(new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        return $"{wordCount} words";
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Hello, my dear cat.";
+        Console.WriteLine(text.WordCount()); 
+    }
+}
