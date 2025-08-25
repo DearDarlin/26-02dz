16302bc00f3032c45b332330cb636a34f459d90a
DearDarlin <darinakovalenko78@gmail.com>
2025-08-25 21:32:30 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+public static class StringExtensions {
+    public static int LastWordLenght(this string str) {
+        if (string.IsNullOrWhiteSpace(str)) return 0;
+        var words = str.Trim().Split(' ');
+        return words[^1].Length;
+    }
+        
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        string example = "Hello Darina";
+        int length = example.LastWordLenght();
+        Console.WriteLine($"The length of the last word is: {length}");
+    }
+}
